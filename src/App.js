9d943d82721f4b06291a09512d1d0d75201078dc
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2023-05-20 05:50:28 +1000

    Theme file and style created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseLine, ThemeProvider } from "@mui/material";
 function App() {
-    return <div className="app"></div>;
+    const [theme, colorMode] = useMode();
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                {/* used to provide consistent styling, resets style to match browser */}
+                <CssBaseLine />
+                <div className="app">
+                    <main className="content"></main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
