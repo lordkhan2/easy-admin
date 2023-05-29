18df1ccc30794700bc6012589b1ebef7708108db
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2023-05-30 07:36:31 +1000

    Multiple tables added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,15 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./scenes/dashboard/dashboard";
 import TopBar from "./scenes/global/topBar";
 import SideBar from "./scenes/global/SideBar";
-// import Team from "./scenes/global/team";
-// import Invoices from "./scenes/global/invoices";
-// import Contacts from "./scenes/global/contacts";
-// import Bar from "./scenes/global/Bar";
-// import Form from "./scenes/global/form";
-// import Line from "./scenes/global/line";
-// import Pie from "./scenes/global/pie";
-// import FAQ from "./scenes/global/faq";
-// import Geography from "./scenes/global/geography";
-// import Calender from "./scenes/global/calender";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -29,9 +28,9 @@ function App() {
                         <TopBar />
                         <Routes>
                             <Route path="/" element={<Dashboard />} />
-                            {/* <Route path="/team" element={<Team />} /> */}
-                            {/* <Route path="/contacts" element={<Contacts />} /> */}
-                            {/* <Route path="/invoices" element={<Invoices />} /> */}
+                            <Route path="/team" element={<Team />} />
+                            <Route path="/contacts" element={<Contacts />} />
+                            <Route path="/invoices" element={<Invoices />} />
                             {/* <Route path="/form" element={<Form />} /> */}
                             {/* <Route path="/bar" element={<Bar />} /> */}
                             {/* <Route path="/pie" element={<Pie />} /> */}
