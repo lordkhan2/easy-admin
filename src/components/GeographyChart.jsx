01e099beee299a97b59b7c6719b84425e42ee785
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2023-06-01 18:11:51 +1000

    Update GeographyChart.jsx

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -1,13 +1,14 @@
-import { ResponsivePie } from "@nivo/pie";
-import { tokens } from "../theme";
 import { useTheme } from "@mui/material";
-import { mockPieData as data } from "../data/mockData";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geoFeatures } from "../data/mockGeoFeatures";
+import { tokens } from "../theme";
+import { mockGeographyData as data } from "../data/mockData";
 
-const PieChart = () => {
+const GeographyChart = ({ isDashboard = false }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     return (
-        <ResponsivePie
+        <ResponsiveChoropleth
             data={data}
             theme={{
                 axis: {
@@ -37,73 +38,48 @@ const PieChart = () => {
                     },
                 },
             }}
-            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-            innerRadius={0.5}
-            padAngle={0.7}
-            cornerRadius={3}
-            activeOuterRadiusOffset={8}
-            borderColor={{
-                from: "color",
-                modifiers: [["darker", 0.2]],
-            }}
-            arcLinkLabelsSkipAngle={10}
-            arcLinkLabelsTextColor={colors.grey[100]}
-            arcLinkLabelsThickness={2}
-            arcLinkLabelsColor={{ from: "color" }}
-            enableArcLabels={false}
-            arcLabelsRadiusOffset={0.4}
-            arcLabelsSkipAngle={7}
-            arcLabelsTextColor={{
-                from: "color",
-                modifiers: [["darker", 2]],
-            }}
-            defs={[
-                {
-                    id: "dots",
-                    type: "patternDots",
-                    background: "inherit",
-                    color: "rgba(255, 255, 255, 0.3)",
-                    size: 4,
-                    padding: 1,
-                    stagger: true,
-                },
-                {
-                    id: "lines",
-                    type: "patternLines",
-                    background: "inherit",
-                    color: "rgba(255, 255, 255, 0.3)",
-                    rotation: -45,
-                    lineWidth: 6,
-                    spacing: 10,
-                },
-            ]}
-            legends={[
-                {
-                    anchor: "bottom",
-                    direction: "row",
-                    justify: false,
-                    translateX: 0,
-                    translateY: 56,
-                    itemsSpacing: 0,
-                    itemWidth: 100,
-                    itemHeight: 18,
-                    itemTextColor: "#999",
-                    itemDirection: "left-to-right",
-                    itemOpacity: 1,
-                    symbolSize: 18,
-                    symbolShape: "circle",
-                    effects: [
-                        {
-                            on: "hover",
-                            style: {
-                                itemTextColor: "#000",
-                            },
-                        },
-                    ],
-                },
-            ]}
+            features={geoFeatures.features}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            domain={[0, 1000000]}
+            unknownColor="#666666"
+            label="properties.name"
+            valueFormat=".2s"
+            projectionScale={isDashboard ? 40 : 150}
+            projectionTranslation={isDashboard ? [0.49, 0.6] : [0.5, 0.5]}
+            projectionRotation={[0, 0, 0]}
+            borderWidth={1.5}
+            borderColor="#ffffff"
+            legends={
+                !isDashboard
+                    ? [
+                          {
+                              anchor: "bottom-left",
+                              direction: "column",
+                              justify: true,
+                              translateX: 20,
+                              translateY: -100,
+                              itemsSpacing: 0,
+                              itemWidth: 94,
+                              itemHeight: 18,
+                              itemDirection: "left-to-right",
+                              itemTextColor: colors.grey[100],
+                              itemOpacity: 0.85,
+                              symbolSize: 18,
+                              effects: [
+                                  {
+                                      on: "hover",
+                                      style: {
+                                          itemTextColor: "#ffffff",
+                                          itemOpacity: 1,
+                                      },
+                                  },
+                              ],
+                          },
+                      ]
+                    : undefined
+            }
         />
     );
 };
 
-export default PieChart;
+export default GeographyChart;
