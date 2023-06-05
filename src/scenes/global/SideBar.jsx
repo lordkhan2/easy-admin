649dec1b461bc26ae778aac9d2b1e0aaf816414b
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2023-06-06 01:53:20 +1000

    Dashboard Completed

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -74,7 +74,7 @@ const Sidebar = () => {
                         {!isCollapsed && (
                             <Box display="flex" justifyContent="space-between" alignItems="center" ml="15px">
                                 <Typography variant="h3" color={colors.grey[100]}>
-                                    ADMINIS
+                                    ADMINI PANEL
                                 </Typography>
                                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                                     <MenuOutlinedIcon />
@@ -86,11 +86,11 @@ const Sidebar = () => {
                     {!isCollapsed && (
                         <Box mb="25px">
                             <Box display="flex" justifyContent="center" alignItems="center">
-                                <img alt="profile-user" width="100px" height="100px" src={`../../assets/user.png`} style={{ cursor: "pointer", borderRadius: "50%" }} />
+                                <img alt="profile-user" width="100px" height="100px" src={`../../assets/user2.png`} style={{ cursor: "pointer", borderRadius: "50%" }} />
                             </Box>
                             <Box textAlign="center">
                                 <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: "10px 0 0 0" }}>
-                                    Farhan
+                                    Jhon Doe
                                 </Typography>
                                 <Typography variant="h5" color={colors.greenAccent[500]}>
                                     Admin
